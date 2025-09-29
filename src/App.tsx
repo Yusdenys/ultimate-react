8f0ff44984d74c32a90f44a75d203fd1411d3f64
Yusdenys Perez <yperez@precisionpay.com>
2025-09-29 10:12:25 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+//import Card, { CardBody } from "./components/Card.tsx";
+import List from "./components/List.tsx";
+import Index from "./components/Button";
+import {useState} from "react";
+import Card from "./components/Card.tsx";
+//import list from "./components/List.tsx";
+import Alert from "./components/Alert";
+
+/*
+function App(){
+    const [data, setData] = useState(["Goku", "Tanjiro", "Chanchito feliz"]);
+
+    /*
+    const [isLoading, setIsLoading ] = useState(false);
+    const handleClick = () => setIsLoading(!isLoading);
+
+    const list: string [] = ["Goku", "Tanjiro", "Chanchito feliz"];
+    const handleSelect = (elemento: string) => {
+        console.log("imprimiendo", elemento);
+    };
+
+    const  contenido = list.length !== 0 &&  (
+        <List data={list} onSelect={handleSelect}/>
+        );
+
+    return (
+        <Card>
+            <CardBody title="Hola Mundo" text="Este es el texto"/>
+            {list.length !== 0 ? (
+                <List data={list} onSelect={handleSelect}/>
+            ) : 'No hay contenido'}
+            <Index isLoading={isLoading} onClick={handleClick}>Hola Mundo</Index>
+        </Card>
+    )
+    */
+
+    /*
+    const addMinion = () => setData([...data, "Minion"]);
+    const removeMinion = () => setData(data.slice(0, -1));
+    return (
+        <Card>
+            <Index isLoading={true} onClick={addMinion}>Agregar</Index>
+            <Index isLoading={false} onClick={removeMinion}>Remove</Index>
+            <List data={data} />
+        </Card>
+    )
+
+
+}
+*/
+
+function App(){
+    const [status, setStatus] = useState(true);
+    const toggleStatus = () => setStatus(!status);
+    return (
+        <Alert status={status} onClick={toggleStatus}>
+            Alert!
+        </Alert>
+    )
+}
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,30 @@
+import {ReactNode} from "react";
+import { FaRegThumbsUp } from "react-icons/fa";
+import styled from "styled-components";
+
+type BtnProps =  {
+    isLoading: boolean;
+}
+
+const Btn = styled.button<BtnProps>`
+    background-color: ${props => props.isLoading ? 'gray' : 'red'};
+    padding: 25px 30px;
+`
+
+type Props = {
+    children: ReactNode;
+    isLoading: boolean;
+    onClick: () => void;
+};
+
+function Button({ children , isLoading, onClick}: Props) {
+    return (
+        <Btn onClick={onClick} disabled={isLoading} isLoading={isLoading}>
+            {isLoading ? 'Cargando...' : children}
+            <FaRegThumbsUp color="blue" size={40}/>
+        </Btn>
+    );
+}
+
+export default Button;
+
